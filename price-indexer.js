432f30dd9fe79dfab592c89508d7e210aa20fad8
Redbubble Applicant <me@example.com>
2021-09-13 00:44:03 +1000

    Index options

--- price-indexer.js
+++ price-indexer.js
@@ -1,13 +1,21 @@
 function index(price, existingIndex) {
   const {
     "product-type": productType,
-    "options": _options,
+    "options": options,
     "base-price": basePrice
   } = price;
 
+  let newTerms = [`product-type:${productType}`];
+
+  for (const [property, values] of Object.entries(options).sort()) {
+    newTerms = newTerms.flatMap(term => values.map(value => [`${term},${property}:${value}`]));
+  }
+
+  const newEntries = Object.fromEntries(newTerms.map(term => [term, basePrice]));
+
   return {
     ...existingIndex,
-    [`product-type:${productType}`]: basePrice
+    ...newEntries
   };
 }
 
--- test/price-indexer.js
+++ test/price-indexer.js
@@ -20,5 +20,88 @@ describe('Index price', function() {
         assert.deepEqual(actual, expected);
       });
     });
+
+    describe('with one option and one value', function() {
+      const price = {
+        "product-type": "sticker",
+        "options": {
+          "size": ["xl"]
+        },
+        "base-price": 1417
+      };
+
+      it('should index name under product-type and options', function() {
+        const expected = { 'product-type:sticker,size:xl': 1417 };
+        const actual = index(price, existingIndex);
+
+        assert.deepEqual(actual, expected);
+      });
+
+    });
+
+    describe('with one option and two values', function() {
+      const price = {
+        "product-type": "sticker",
+        "options": {
+          "size": ["large", "xl"]
+        },
+        "base-price": 1417
+      };
+
+      it('should index name under product-type and options', function() {
+        const expected = {
+          'product-type:sticker,size:xl': 1417,
+          'product-type:sticker,size:large': 1417
+        };
+        const actual = index(price, existingIndex);
+
+        assert.deepEqual(actual, expected);
+      });
+
+    });
+
+    describe('with two sorted options', function() {
+      const price = {
+        "product-type": "hoodie",
+        "options": {
+          "colour": ["dark"],
+          "size": ["xl", "2xl", "3xl"]
+        },
+        "base-price": 4368
+      };
+
+      it('should index name under product-type and options', function() {
+        const expected = {
+          'product-type:hoodie,colour:dark,size:xl': 4368,
+          'product-type:hoodie,colour:dark,size:2xl': 4368,
+          'product-type:hoodie,colour:dark,size:3xl': 4368
+        };
+        const actual = index(price, existingIndex);
+
+        assert.deepEqual(actual, expected);
+      });
+
+    });
+
+    describe('with two unsorted options', function() {
+      const price = {
+        "product-type": "hoodie",
+        "options": {
+          "size": ["large"],
+          "colour": ["white"]
+        },
+        "base-price": 3848
+      };
+
+      it('should index name under product-type and options', function() {
+        const expected = {
+          'product-type:hoodie,colour:white,size:large': 3848
+        };
+        const actual = index(price, existingIndex);
+
+        assert.deepEqual(actual, expected);
+      });
+
+    });
   });
 });
